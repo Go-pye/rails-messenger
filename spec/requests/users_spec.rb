a9da13228bf589cf93ba13ae94126baeaa59b9ca
sean delaney <fattyeleven@proton.me>
2023-12-24 15:54:58 -0800

    test: add basic request spec for users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Home", type: :request do
-  describe "GET /" do
+RSpec.describe "Users", type: :request do
+  describe "GET /index" do
     it "returns http success" do
-      get "/"
+      get "/users/index"
       expect(response).to have_http_status(:success)
     end
   end
