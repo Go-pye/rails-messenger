445bb8e8cc50f4a623c245f11948d5a86c7d4cc7
sean delaney <fattyeleven@proton.me>
2023-12-24 16:38:39 -0800

    test: add test for authentication

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/users/index"
-      expect(response).to have_http_status(:success)
+    context 'user is not logged in' do
+      it 'redirects to user sign in page' do
+        get "/users/index"
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context 'user is signed in' do
+      before do
+        user = FactoryBot.create(:user)
+        sign_in user
+      end
+      it 'loads users index page' do
+        get "/users/index"
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
