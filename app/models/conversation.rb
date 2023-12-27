4b66a4706a27ee50b6e2c5a9e61bc4cbccbaab39
sean delaney <fattyeleven@proton.me>
2023-12-27 11:41:21 -0800

    feat: add scope to conversation to find recent messages for user

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,4 +2,12 @@ class Conversation < ApplicationRecord
   belongs_to :participant1, class_name: 'User'
   belongs_to :participant2, class_name: 'User'
   has_many :messages
+
+  scope :with_recent_messages, -> (user_id) {
+    joins(:messages)
+    .where('messages.created_at > ?', 60.minutes.ago)
+    .where('participant1_id = ? OR participant2_id = ?', user_id, user_id)
+    .distinct
+  }
+
 end
