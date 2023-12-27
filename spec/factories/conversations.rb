1640a1260df128b4577d4fc1db0d62e3c05af4ee
sean delaney <fattyeleven@proton.me>
2023-12-27 09:38:49 -0800

    test: add request spec for messages

--- spec/factories/conversations.rb
+++ spec/factories/conversations.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :conversation do
-    
+    association :participant1, factory: :user
+    association :participant2, factory: :user
   end
 end
---
+++ spec/requests/messages_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "Messages", type: :request do
+  let(:participant1) { create(:user) }
+  let(:participant2) { create(:user) }
+  let(:conversation) { create(:conversation) }
+  let(:message_params) { { message: { content: "heeeeeyyyyy" } } }
+
+  before do
+    sign_in participant1
+  end
+
+  describe "POST /create" do
+    context "with valid parameters" do
+      it "creates a new Message" do
+        expect {
+          post conversation_messages_path(conversation), params: message_params
+        }.to change(Message, :count).by(1)
+      end
+
+      it "returns a success response" do
+        post conversation_messages_path(conversation), params: message_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not create a new Message" do
+        expect {
+          post conversation_messages_path(conversation), params: { message: { content: nil } }
+        }.to change(Message, :count).by(0)
+      end
+
+      it "returns an error response" do
+        post conversation_messages_path(conversation), params: { message: { content: nil } }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+end
